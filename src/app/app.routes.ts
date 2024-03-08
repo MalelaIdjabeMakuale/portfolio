0820a1aecf3547c0e0444ad80a6bcd4dc9bfae06
bokombe <malela.im90@gmail.com>
2024-03-08 20:23:14 +0100

    version casi dedefinitiva. faltan fuentes y hover

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WorksComponent } from './pages/works/works.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'aboutme', component: AboutMeComponent },
+  {path: 'skills', component: SkillsComponent}, 
+  {path: '', component: HomeComponent}, {path: 'works', component: WorksComponent}
+];
